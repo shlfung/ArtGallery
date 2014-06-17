60ccb3a3f7bc3031c6fc49299b457289f49b07d9
Stephen Fung <stephen.shlfung@gmail.com>
2014-06-16 23:21:32 -0700

    Adding Sculpture

--- gallerydb.php
+++ gallerydb.php
@@ -336,10 +336,10 @@ $fnameErr = $lnameErr =$emailErr = $phoneErr= "";
 
     if (isset($_POST['apaintingsql'])) {
 
-     //$result = executePlainSQL($link, "SELECT A.serial_number FROM Art A WHERE A.serial_number >= ALL (SELECT Art.serial_number FROM Art)");
+     $result = executePlainSQL($link, "SELECT A.serial_number FROM Art A WHERE A.serial_number >= ALL (SELECT Art.serial_number FROM Art)");
      
      // Find the largest serial number and add 1 to be the new serial number
-     $result = executePlainSQL($link, "SELECT MAX(A.serial_number) FROM Art A");
+     //$result = executePlainSQL($link, "SELECT MAX(A.serial_number) FROM Art A");
      
      // Grab and Generate the new SQL Integer from the SQL Statement
      while($row = mysqli_fetch_array($result)) {
@@ -382,6 +382,98 @@ $fnameErr = $lnameErr =$emailErr = $phoneErr= "";
       echo $newSerial;
 
       $commission = $_POST['pcommission'];
+
+      $query3="INSERT INTO supplies VALUES('$artistFname', '$artistLname', $artistPhone, $commission, $newSerial)";
+
+      $success3 = executePlainSQL($link, $query3);
+      if ($success3) {
+          echo "<br>";
+          echo "Statement: <br>".$query3."<br>Executed successfully.";  
+      }
+    }
+
+    // adding a sculpture
+
+    if (isset($_GET['asculpture']) || isset($_POST['asculpturesql'])) {
+     ?>
+     <form align="center" action='http://localhost/cs304/gallerydb.php' method="post">
+         Title: <input type="text" name="stitle">
+         Price: <input type="text" name="sprice"> <br>
+         Material: <input type="text" name="smaterial">
+         Style: <input type="text" name="sstyle"> 
+         Image Link: <input type="text" name="surl"> <br>
+         Comission Rate: <input type='number' name='scommission'>
+         Choose Artist: <select name="select_artist">
+    <?php
+      
+    //Generating Artist Selection    	
+	$theArtist = executePlainSQL($link,"SELECT * FROM artists");
+		while($row = mysqli_fetch_array($theArtist)) {
+			$fname =  $row['fname'] ;
+			$lname =  $row['lname'] ;
+			$phone =  $row['phone'] ;
+			echo "<option value=".$phone.">" .$fname. ','
+									 .$lname. ','
+									 .$phone. 
+							"</option>";
+		}
+	echo "</select> ";
+    ?>
+
+     <button name='asculpturesql' type='submit' value='true'>Add</button>
+     </form>
+
+    <?php
+    }
+
+    if (isset($_POST['asculpturesql'])) {
+
+     $result = executePlainSQL($link, "SELECT A.serial_number FROM Art A WHERE A.serial_number >= ALL (SELECT Art.serial_number FROM Art)");
+     
+     // Find the largest serial number and add 1 to be the new serial number
+     //$result = executePlainSQL($link, "SELECT MAX(A.serial_number) FROM Art A");
+     
+     // Grab and Generate the new SQL Integer from the SQL Statement
+     while($row = mysqli_fetch_array($result)) {
+         echo $row['serial_number'];
+         $newSerial = $row['serial_number'];
+         echo "<br>";
+     }
+     $newSerial++;
+     echo $newSerial;
+     echo "<br>";
+
+     $query="INSERT INTO art VALUES ($newSerial,'"
+     .$_POST['stitle']."','"
+     .$_POST['sprice']."','"
+     .$_POST['surl']."');";
+     $query2="INSERT INTO sculpture VALUES($newSerial,'"
+     .$_POST['smaterial']."','"
+     .$_POST['sstyle']."');";
+ 
+     $success =  executePlainSQL($link, $query);
+     $success2 = executePlainSQL($link, $query2);
+     if ($success and $success2) {
+         echo "Statement: <br>".$query."<br>Executed successfully.";
+         echo "Statement: <br>".$query2."<br>Executed successfully.";        
+     }
+
+     // Insert the Art and Artist to the Supplies Table
+     $artistInfo = executePlainSQL($link,"SELECT * FROM artists WHERE phone = '".$_POST['select_artist']."'");
+      while($row=mysqli_fetch_array($artistInfo)) {
+
+            $artistFname = $row['fname'];
+            $artistLname = $row['lname'];
+            $artistPhone = $row['phone'];   
+        }
+      
+      echo "<br>";
+      echo $artistFname;
+      echo $artistLname;
+      echo $artistPhone;
+      echo $newSerial;
+
+      $commission = $_POST['scommission'];
 
       $query3="INSERT INTO supplies VALUES('$artistFname', '$artistLname', $artistPhone, $commission, $newSerial)";
 
